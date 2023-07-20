7986081955140bb22f749518844347a82a281487
Patrick Oser <patrick.oser148@gmail.com>
2023-07-20 16:20:32 -0400

    Webpack setup and home.js complete.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,21 @@
 export { makeHeader, makeHomeContent };
 import Puzzles from "../assets/puzzles.jpg";
 
-const contentContainer = document.querySelector("#content");
-
 function makeHeader() {
   const header = document.createElement("div");
   const homeBtn = document.createElement("button");
   const menuBtn = document.createElement("button");
   const aboutBtn = document.createElement("button");
+  const content = document.createElement("div");
 
   header.classList.add("headerContainer");
   homeBtn.classList.add("homeBtn");
   menuBtn.classList.add("menuBtn");
   aboutBtn.classList.add("aboutBtn");
+  content.id = "content";
 
-  contentContainer.appendChild(header);
+  document.body.appendChild(content);
+  content.appendChild(header);
   header.appendChild(homeBtn);
   header.appendChild(menuBtn);
   header.appendChild(aboutBtn);
@@ -29,7 +30,6 @@ function makeHomeContent() {
   const homeContentTitle = document.createElement("div");
   const homeContentImgContainer = document.createElement("div");
   const puzzlesImg = new Image();
-  puzzlesImg.src = Puzzles;
   const homeContentBlurb = document.createElement("div");
 
   homeContentContainer.classList.add("homeContentContainer");
@@ -38,11 +38,14 @@ function makeHomeContent() {
   puzzlesImg.classList.add("puzzlesImg");
   homeContentBlurb.classList.add("homeContentBlurb");
 
-  contentContainer.appendChild(homeContentContainer);
+  content.appendChild(homeContentContainer);
   homeContentContainer.appendChild(homeContentTitle);
   homeContentContainer.appendChild(homeContentImgContainer);
   homeContentContainer.appendChild(puzzlesImg);
   homeContentContainer.appendChild(homeContentBlurb);
 
   homeContentTitle.innerHTML = "<h1>Puzzles Bar & Grill</h1>";
+  puzzlesImg.src = Puzzles;
+  homeContentBlurb.textContent =
+    'Welcome to your favorite neighborhood establishment to drink, be merry, and make memories! Looking for a drink? Food? A friendly face? Come on down to Puzzles where the only question you will be left with is... "Why is the name of this place Puzzles?';
 }
