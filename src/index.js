43803207d73a93e72f5da191d06d80499198e7d3
Patrick Oser <patrick.oser148@gmail.com>
2023-07-20 12:46:49 -0400

    testing.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { makeHeader, makeHomeContent } from "./pages/home.js";
 import "./style.css";
+import printMe from "./print.js";
 
 makeHeader();
 makeHomeContent();
+printMe();
 
 console.log("Hello");
